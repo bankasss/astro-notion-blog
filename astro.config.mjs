1d8dc59b67efb7692763359d9815dbfde889d4e4
bankas <sakubankas@gmail.com>
2023-04-26 22:36:34 +0900

    ogpといいねボタン修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import CoverImageDownloader from './src/integrations/cover-image-downloader';
 import CustomIconDownloader from './src/integrations/custom-icon-downloader';
 import FeaturedImageDownloader from './src/integrations/featured-image-downloader';
 import PublicNotionCopier from './src/integrations/public-notion-copier';
+import vercel from "@astrojs/vercel/serverless";
 
 const getSite = function () {
   if (CUSTOM_DOMAIN) {
@@ -35,10 +36,7 @@ const getSite = function () {
 export default defineConfig({
   site: getSite(),
   base: BASE_PATH,
-  integrations: [
-    CoverImageDownloader(),
-    CustomIconDownloader(),
-    FeaturedImageDownloader(),
-    PublicNotionCopier(),
-  ],
+  integrations: [CoverImageDownloader(), CustomIconDownloader(), FeaturedImageDownloader(), PublicNotionCopier()],
+  output: "server",
+  adapter: vercel()
 });
